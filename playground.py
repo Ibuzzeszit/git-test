a4be02afb7f54c5fd7f9375db6bbbdeb1da145c9
Blake <21008916+Ibuzzeszit@users.noreply.github.com>
2023-12-19 00:02:50 +1100

    added roman numeral conversion method

--- playground.py
+++ playground.py
@@ -1,4 +1,4 @@
-class Playground:
+class playground:
 
     def Palindrome(x:int) -> bool:
         if x < 0:
@@ -17,4 +17,35 @@ class Playground:
                 return False
         return True
 
-print(Playground.Palindrome(2154512))
+
+    def romanToInt(s:str) -> int:
+        romanNum = []
+        total = 0
+        for i in s:
+            if i == "I":
+                romanNum.append(1)
+            elif i == "V":
+                romanNum.append(5)
+            elif i == "X":
+                romanNum.append(10)
+            elif i == "L":
+                romanNum.append(50)
+            elif i == "C":
+                romanNum.append(100)
+            elif i == "D":
+                romanNum.append(500)
+            else:
+                romanNum.append(1000)
+            print(romanNum)
+
+            lenRomanNum = len(romanNum)
+            print(lenRomanNum)
+            
+        for i, x in enumerate(romanNum):
+            if i != lenRomanNum-1:
+                if romanNum[i] < romanNum[i+1]:
+                    romanNum[i] = -romanNum[i]
+            total = total + romanNum[i]
+        return total
+    
+print(playground.romanToInt("XCIX"))
